1a1630875df0d1b1f4b93b9e733505cd120bcaab
andrea padgett + ariel spear + kai millner <agpadgett89@gmail.com>
2015-09-17 14:25:10 -0700

    show cards and get values

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+
+
+
 // function card(val, suit){
 //   this.suit = suit;
 //   this.val = val;
@@ -50,5 +53,80 @@ function makeDealer(){
 
 function giveCard(deck, player){
   player.push(deck[0]);
-  deck.splice(0, 0);
+  deck.splice(0, 1);
+}
+
+function showPlayerCards(player){
+  var playerVisibleCards = []
+  for(var i = 0; i < player.length; i ++){
+    var card = player [i];
+    playerVisibleCards.push(card);
+  }
+  return playerVisibleCards;
+}
+
+function showDealerCards(dealer){
+  var dealerVisibleCards = []
+  for(var i = 1; i < dealer.length; i ++){
+    var card = dealer [i];
+    dealerVisibleCards.push(card);
+  }
+  return dealerVisibleCards;
+} //this is the VISIBLE cards the dealer has. One is hidden, it's not in the dealerVisibleCards array
+
+function getNumericValue(card){
+  var numValue;
+  if (card[0] === "two"){
+    numValue = 2;
+  }
+
+  if (card[0] === "three"){
+    numValue = 3;
+  }
+
+  if (card[0] === "four"){
+    numValue = 4;
+  }
+
+  if (card[0] === "five"){
+    numValue = 5;
+  }
+
+  if (card[0] === "six"){
+    numValue = 6;
+  }
+
+  if (card[0] === "seven"){
+    numValue = 7;
+  }
+
+  if (card[0] === "eight"){
+    numValue = 8;
+  }
+
+  if (card[0] === "nine"){
+    numValue = 9;
+  }
+
+  if (card[0] === "ten"){
+    numValue = 10;
+  }
+
+  if (card[0] === "jack"){
+    numValue = 10;
+  }
+
+  if (card[0] === "queen"){
+    numValue = 10;
+  }
+
+  if (card[0] === "king"){
+    numValue = 10;
+  }
+
+  if (card[0] === "ace"){
+    numValue = 11;
+  }
+
+  return numValue;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -33,6 +33,36 @@ describe('giveCard', function(){
   var player = makePlayer();
   var deck = shuffleDeck(makeDeck());
   giveCard(deck, player);
-  expect(player.length === 1); //.to.equal won't work here. WE don't know why
+  expect(player.length === 1); //.to.equal won't work here. We don't know why
   })
 });
+
+describe('showPlayerCards', function(){
+  it('show all cards in players hand', function(){
+    var deck = makeDeck();
+    var player = makePlayer();
+    giveCard(deck, player);
+    var cards = showPlayerCards(player);
+    expect(cards).to.eql([["ace", "hearts"]]);
+  })
+});
+
+describe('showDealerCards', function(){
+  it('show all cards in dealer hand, but one', function(){
+    var deck = makeDeck();
+    var dealer = makeDealer();
+    giveCard(deck, dealer);
+    giveCard(deck,dealer);
+    var cards = showDealerCards(dealer);
+    expect(cards).to.eql([["ace", "clubs"]]);
+  })
+});
+
+describe('getNumericValue', function(){
+  it('shows the numeric value of a card', function(){
+    var deck = makeDeck();
+    var card = deck[0];
+    var integ = getNumericValue(card);
+    expect(integ === 11);
+  })
+})
